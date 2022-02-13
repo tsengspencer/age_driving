74a017a1ed00d76340f80e3bface619dafff451e
Specner Tseng <yuyun@gapp.nthu.edu.tw>
2022-02-13 20:57:29 +0800

    add United State

--- age_driving.py
+++ age_driving.py
@@ -5,3 +5,10 @@ if country == "Korea":
 		print(" You are allowed to take driver license test")
 	else:
 		print("You are too young to take! Young man!")
+elif country == "U.S.A":
+	if age >= 16:
+		print("Wow! you can take the test NOW!")
+	else:
+		print("Man! you're close to taking the exam! Be patient!")
+else:
+  print("Sytem Fail to indentify, Soooooorry!")
